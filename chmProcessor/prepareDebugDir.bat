fa482d6aa1065d3dc61e3b46d72aa29ddf6c9c7b
tonibo <tonibo@a4511d3f-d600-4539-bb2b-08a408a67561>
2011-04-19 10:10:12 +0000

    - Tasks list changed - Directory names on prepareDebugDir.bat changed

--- chmProcessor/prepareDebugDir.bat
+++ chmProcessor/prepareDebugDir.bat
@@ -1,20 +1,20 @@
-COPY chmProcessor.chm ProcesadorHtml\bin\Debug
-COPY license.txt ProcesadorHtml\bin\Debug
-MKDIR ProcesadorHtml\bin\Debug\webFiles
-COPY webFiles ProcesadorHtml\bin\Debug\webFiles
-MKDIR ProcesadorHtml\bin\Debug\webTranslations
-COPY webTranslations ProcesadorHtml\bin\Debug\webTranslations
-copy ProcesadorHtml\dialog-information.png ProcesadorHtml\bin\Debug
-copy ProcesadorHtml\dialog-error.png ProcesadorHtml\bin\Debug
-copy searchdb.sql ProcesadorHtml\bin\Debug
-copy web\chmProcessorDocumentation.pdf ProcesadorHtml\bin\Debug
+COPY chmProcessor.chm ChmProcessor\bin\Debug
+COPY license.txt ChmProcessor\bin\Debug
+MKDIR ChmProcessor\bin\Debug\webFiles
+COPY webFiles ChmProcessor\bin\Debug\webFiles
+MKDIR ChmProcessor\bin\Debug\webTranslations
+COPY webTranslations ChmProcessor\bin\Debug\webTranslations
+copy ChmProcessor\dialog-information.png ChmProcessor\bin\Debug
+copy ChmProcessor\dialog-error.png ChmProcessor\bin\Debug
+copy searchdb.sql ChmProcessor\bin\Debug
+copy web\chmProcessorDocumentation.pdf ChmProcessor\bin\Debug
 
 REM PREPARE SEARCH FILES:
-MKDIR ProcesadorHtml\bin\Debug\searchFiles
-MKDIR ProcesadorHtml\bin\Debug\searchFiles\Bin
-COPY WebFullTextSearch\Bin ProcesadorHtml\bin\Debug\searchFiles\Bin
-COPY WebFullTextSearch\search.aspx ProcesadorHtml\bin\Debug\searchFiles
-COPY WebFullTextSearch\search.aspx.cs ProcesadorHtml\bin\Debug\searchFiles
-COPY WebFullTextSearch\Web.Config ProcesadorHtml\bin\Debug\searchFiles
+MKDIR ChmProcessor\bin\Debug\searchFiles
+MKDIR ChmProcessor\bin\Debug\searchFiles\Bin
+COPY WebFullTextSearch\Bin ChmProcessor\bin\Debug\searchFiles\Bin
+COPY WebFullTextSearch\search.aspx ChmProcessor\bin\Debug\searchFiles
+COPY WebFullTextSearch\search.aspx.cs ChmProcessor\bin\Debug\searchFiles
+COPY WebFullTextSearch\Web.Config ChmProcessor\bin\Debug\searchFiles
 
 PAUSE
