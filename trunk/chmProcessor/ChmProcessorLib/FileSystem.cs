8451b80b2c2da785a2628bcf31c352d323fd3a66
tonibo <tonibo@a4511d3f-d600-4539-bb2b-08a408a67561>
2011-07-16 09:00:30 +0000

    PathRelativePathToW was not working. Changed again to PathRelativePathTo, pending to check with unicode names.

--- trunk/chmProcessor/ChmProcessorLib/FileSystem.cs
+++ trunk/chmProcessor/ChmProcessorLib/FileSystem.cs
@@ -45,7 +45,7 @@ namespace ChmProcessorLib
         /// </summary>
         /// <returns></returns>
         [DllImport("shlwapi.dll", SetLastError = true)]
-        private static extern int PathRelativePathToW(StringBuilder pszPath,
+        private static extern int PathRelativePathTo(StringBuilder pszPath,
             string pszFrom, int dwAttrFrom, string pszTo, int dwAttrTo);
 
         /// <summary>
@@ -90,7 +90,7 @@ namespace ChmProcessorLib
             int toAttr = GetPathAttribute(toPath, toType);
 
             StringBuilder path = new StringBuilder(260); // MAX_PATH
-            if (PathRelativePathToW(
+            if (PathRelativePathTo(
                 path,
                 fromPath,
                 fromAttr,
