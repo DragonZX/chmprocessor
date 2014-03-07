c3964292320a2043379d03b7ca22fd5088d13766
tonibo <tonibo@a4511d3f-d600-4539-bb2b-08a408a67561>
2014-03-07 07:03:57 +0000

    - Test changes for command line

---
+++ chmProcessor/tests/CmdLineTest/CmdLineTestDoc.bat
@@ -0,0 +1,3 @@
+REM Test project generation with a Word file
+..\..\ChmProcessor\bin\x86\Debug\ChmProcessor.exe ..\Test1.doc /g /y /e
+PAUSE
--- chmProcessor/tests/CmdLineTest/CmdLineTestWhc.bat
+++ chmProcessor/tests/CmdLineTest/CmdLineTestWhc.bat
@@ -1,2 +1,3 @@
-REM Test project generation
+REM Test project generation with a Chmprocessor project
 ..\..\ChmProcessor\bin\x86\Debug\ChmProcessor.exe ..\StandardTest\test.WHC /g /y /e
+PAUSE
